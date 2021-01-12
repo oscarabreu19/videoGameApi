58b21caf9fe8c119b653fe044fdab626eb0d7d51
Oscar Galindez <oscarabreu19@gmail.com>
2021-01-12 00:46:31 -0300

    [wip] Se hace un mock para la funcion 'createGameSystem' en el modulo 'gamesystem.service'

---
+++ api/services/gamesystem.service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+//////////////////////////////////////////////////////
+// PUBLIC FUNCTIONS
+//////////////////////////////////////////////////////
+
+function createGameSystem(params) {
+    // TODO: Este metodo tiene que ir al repositorio a crear un nuevo Game System
+    params.id = 123;
+    return params;
+}
+
+module.exports = {
+    createGameSystem
+};
