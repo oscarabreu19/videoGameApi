c49e667e84e37f2a858897d7ad53be044e82df34
Oscar Galindez <oscarabreu19@gmail.com>
2021-01-12 10:49:44 -0300

    [add] Se crea en el servicio de game system la funcion 'getGameSystemByName' con la que se consulta en el repository

--- api/services/gamesystem.service.js
+++ api/services/gamesystem.service.js
@@ -14,6 +14,10 @@ const gameSystemRepository = require('../repositories/gamesystem.repository.js')
 // PUBLIC FUNCTIONS
 //////////////////////////////////////////////////////
 
+function getGameSystemByName(name) {
+    return gameSystemRepository.getGameSystemByName(name);
+}
+
 function createGameSystem(params) {
     // TODO: Este metodo tiene que ir al repositorio a crear un nuevo Game System
     params.id = 123;
